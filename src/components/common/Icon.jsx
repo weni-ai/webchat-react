34cae6c66f5f5d18b7c394405cb1eb7f7c61194d
Aldemylla <aldemylla@gmail.com>
2025-10-10 14:58:51 -0300

    feat: Integrate Material Symbols icon library and enhance Icon component with new props and styling

--- src/components/common/Icon.jsx
+++ src/components/common/Icon.jsx
@@ -1,32 +1,63 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import 'material-symbols'
+
+import './Icon.scss'
 
 /**
- * Icon - Icon component wrapper
- * TODO: Support different icon libraries (SVG, icon fonts)
- * TODO: Add size variants
- * TODO: Add color customization
- * TODO: Implement common icons (send, attach, close, etc.)
+ * Icon - Icon component using Material Symbols
+ * 
+ * Uses Material Symbols icon font library for consistent, scalable icons.
+ * Icon names should match Material Symbols icon names (e.g., 'send', 'close', 'chat', 'attach_file', 'mic')
+ * 
+ * @example
+ * <Icon name="send" size="medium" />
+ * <Icon name="close" size="large" filled />
  */
-export function Icon({ name, size = 'medium', color = 'currentColor', className = '', ...props }) {
-  // TODO: Implement icon rendering based on icon library
-  // TODO: Support custom SVG icons
+export function Icon({ 
+  name = '', 
+  size = 'medium', 
+  color = 'currentColor',
+  filled = false,
+  outlined = false,
+  weight = 400,
+  grade = 0,
+  opticalSize = 24,
+  className = '', 
+  ...props 
+}) {
+  const variantClass = outlined ? 'material-symbols-outlined' : 'material-symbols-rounded'
+  
+  const style = {
+    color,
+    fontVariationSettings: `'FILL' ${filled ? 1 : 0}, 'wght' ${weight}, 'GRAD' ${grade}, 'opsz' ${opticalSize}`
+  }
   
   return (
     <span 
-      className={`weni-icon weni-icon-${name} weni-icon-${size} ${className}`}
-      style={{ color }}
+      className={`weni-icon ${variantClass} weni-icon--${size} ${className}`}
+      style={style}
+      aria-hidden="true"
       {...props}
     >
-      {/* TODO: Render icon based on name */}
+      {name}
     </span>
   )
 }
 
 Icon.propTypes = {
+  /** Material Symbols icon name (e.g., 'send', 'close', 'chat') */
   name: PropTypes.string.isRequired,
   size: PropTypes.oneOf(['small', 'medium', 'large']),
   color: PropTypes.string,
+  filled: PropTypes.bool,
+  outlined: PropTypes.bool,
+  /** Font weight (100-700) */
+  weight: PropTypes.number,
+  /** Grade adjustment (-25 to 200) */
+  grade: PropTypes.number,
+  /** Optical size for different display sizes */
+  opticalSize: PropTypes.number,
   className: PropTypes.string
 }
 
