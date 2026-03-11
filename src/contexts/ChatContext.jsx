7c01eb1e6e7d4ac22c51d7c9d62b863c99386d8d
Matheus Cristian <mathues@email.com>
2026-03-11 10:58:11 -0300

    fix(embedded-mode): fixes ESLint errors

--- src/contexts/ChatContext.jsx
+++ src/contexts/ChatContext.jsx
@@ -113,7 +113,7 @@ export function ChatProvider({ children, config }) {
   const [cameraDevices, setCameraDevices] = useState([]);
 
   // UI-specific state
-  const [isChatOpen, setIsChatOpen] = useState(!!(mergedConfig.startFullScreen));
+  const [isChatOpen, setIsChatOpen] = useState(!!mergedConfig.startFullScreen);
   const [isChatFullscreen, setIsChatFullscreen] = useState(
     mergedConfig.startFullScreen,
   );
