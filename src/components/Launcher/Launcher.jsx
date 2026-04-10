a6934ab884ce2be2c686b6412fadec0259acd57e
Matheus Cristian <mathues@email.com>
2026-04-10 13:13:56 -0300

    fix: changes voice mode icon on launcher

--- src/components/Launcher/Launcher.jsx
+++ src/components/Launcher/Launcher.jsx
@@ -43,7 +43,7 @@ export function Launcher() {
             <span className="weni-launcher__separator" />
 
             <Icon
-              name="mic"
+              name="graphic_eq"
               size="medium"
             />
           </>
